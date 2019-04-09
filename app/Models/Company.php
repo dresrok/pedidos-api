b356452b0b3f04ddd73e52fc4e2fe98a94efb266
diegoavilap <diegoavilapava@hotmail.com>
2019-04-09 15:02:03 -0500

    Models for Module B

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,50 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Illuminate\Database\Eloquent\Relations\{BelongsToMany, HasMany};
 
 class Company extends Model
 {
-    //
+    use SoftDeletes;
+
+    const CREATED_AT = 'company_network_created_at';
+    const UPDATED_AT = 'company_network_updated_at';
+    const DELETED_AT = 'company_network_deleted_at';
+
+    protected $table = 'b_companies';
+    protected $primaryKey = 'company_id';
+
+    protected $fillable = [
+        'company_legal_name',
+        'company_commercial_name',
+        'company_slug',
+        'company_image',
+        'city',
+        'company_is_certified'
+    ];
+    
+    protected $dates = [
+        'company_network_created_at',
+        'company_network_updated_at',
+        'company_network_deleted_at'
+    ];
+
+    public function offices() : HasMany
+    {
+        return $this->hasMany(Ofice::class, 'company_id', 'company_id');
+    }
+
+    public function socialNetworks() : BelongsToMany
+    {
+        return $this->belongsToMany(SocialNetwork::class, 'b_company_social_networks', 'company_id', 'social_network_id');
+    }
+
+    public function businessTypes() : BelongsToMany
+    {
+        return $this->belongsToMany(BusinessType::class, 'b_company_business_types', 'company_id', 'business_type_id');
+    }
+
+    
 }
--- app/Models/Office.php
+++ app/Models/Office.php
@@ -3,8 +3,43 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Illuminate\Database\Eloquent\Relations\{HasMany};
+
+use Illuminate\Database\Eloquent\Relations\{BelongsTo};
 
 class Office extends Model
 {
-    //
+    use SoftDeletes;
+
+    const CREATED_AT = 'office_created_at';
+    const UPDATED_AT = 'office_updated_at';
+    const DELETED_AT = 'office_deleted_at';
+
+    protected $table = 'b_offices';
+    protected $primaryKey = 'office_id';
+
+    protected $fillable = [
+        'office_name',
+        'office_email',
+        'office_open_from',
+        'office_open_to',
+        'office_delivery_time',
+        'office_minimum_order_price',
+        'city',
+        'company_id'
+    ];
+    
+    protected $dates = [
+        'office_created_at',
+        'office_updated_at',
+        'office_deleted_at'
+    ];
+
+
+    public function company() : BelongsTo
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
 }
