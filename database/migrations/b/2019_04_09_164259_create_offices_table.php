bb1ca0025a9a17fe44b3ccf5d41b31d5e5eb3650
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-05-08 16:51:31 -0500

    Changing TimeZone and time column to office

--- database/migrations/b/2019_04_09_164259_create_offices_table.php
+++ database/migrations/b/2019_04_09_164259_create_offices_table.php
@@ -18,8 +18,8 @@ class CreateOfficesTable extends Migration
 
             $table->string('office_name', 256);
             $table->string('office_email', 64)->nullable();
-            $table->string('office_open_from', 8)->nullable();
-            $table->string('office_open_to', 8)->nullable();
+            $table->time('office_open_from')->nullable();
+            $table->time('office_open_to')->nullable();
             $table->string('office_delivery_time', 16)->nullable();
             $table->double('office_minimum_order_price', 8, 2)->nullable();
             $table->string('city', 32);
